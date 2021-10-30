2f62c3daa67408ce99889bb62dbd2be8371bd859
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-10-30 12:23:51 +0530

    set webhook endpoint

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ const express = require("express");
 const https = require("https");
 const app = express();
 const bodyParser = require("body-parser");
+const webhook = require("./webhook");
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static("Public"));
+
+//handled CORS
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Methods", "*");
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.use("/webhook", webhook);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("server started");
